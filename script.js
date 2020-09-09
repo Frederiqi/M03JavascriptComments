c80b64a4948adef61fc1ac99e5f71fc865d8d605
Frederiqi <68908971+Frederiqi@users.noreply.github.com>
2020-09-09 16:02:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hallo Winc Academy studenten")
+/*
+//Dit is een grote som:
+console.log("Die opmerking over de grote som zie je bij het runnen niet staan, daarvoor is het een opmerking")
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("division:", division); */
+console.log("Nee, nu zien we de bovenstaande sommen niet meer staan. Ze zijn opgenomen in de opmerkingen en dan zie je de uitkomsten bij het runnen niet meer.")
